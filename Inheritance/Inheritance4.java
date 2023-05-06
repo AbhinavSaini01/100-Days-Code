f8a6b2a00ecc3ecb78439f7a48759d7241faa245
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-05-06 23:45:11 +0530

    Add files via upload

---
+++ Inheritance/Inheritance4.java
@@ -0,0 +1,47 @@
+package Inheritance;
+
+
+class Demo1  //Multilevel inheritance 
+{
+  void disp()
+  {
+     System.out.println("Disp wrttien in demo1 class");
+  }
+}
+class Demo2 extends Demo1
+{
+
+}
+class Demo3 extends Demo2
+{
+
+}
+
+
+
+//class Parent1 //extends Object   **Multiple inheritance : In which one child class call two parent class which is not possible in java**
+//{
+// int age=18;
+//}
+//class Parent2 //extends Object
+//{
+// int age=19;
+//}
+//
+//class Child extends Parent1 , extends Parent2
+//{
+//
+//}
+
+
+
+public class Inheritance4 
+{
+ public static void main(String[] args) 
+ {
+      Demo3 d=new Demo3();
+      d.disp();
+ }
+ 
+}
+
