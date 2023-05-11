1c9784c994d51279cfd3689dc3bab2d0c39e1482
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-05-11 23:00:41 +0530

    Add files via upload

---
+++ Interface/Interface2.java
@@ -0,0 +1,31 @@
+package Interface;
+
+interface A
+{
+    void show(); 
+}
+interface X
+{
+    void abc();
+}
+class B implements A,X
+{
+    public void show()
+    {
+        System.out.println("in show");
+    }
+    public void abc()
+    {
+        System.out.println("in abc");
+    }
+}
+
+public class  Interface2 {
+    public static void main(String[] args) {
+       B obj = new B();
+       obj.show();
+       obj.abc();
+      
+    }
+}
+
