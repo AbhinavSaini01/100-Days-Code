7ef59c9aaeaea719bc9d8245706f8e344040a02b
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-05-19 23:36:26 +0530

    Add files via upload

---
+++ ExceptionHandling/CustomException.java
@@ -0,0 +1,48 @@
+package ExceptionHandling;
+
+class MyException extends Exception
+{
+	public MyException()
+	{
+		
+	}
+	public MyException(String msg)
+	{
+		super(msg);
+	}
+};
+
+public class CustomException{
+
+	public static void main(String[] args)
+	{
+		
+
+		int num1=6;
+		int num2=-2;
+		
+		try
+		{
+			
+		if(num2<0)
+		{
+			Exception e=new MyException("Negative Number");
+			throw e;
+		}
+		else
+		{
+		int result= num1/num2;
+		System.out.println(result);
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Enter a valid number" + e);
+		}
+	}
+
+		
+
+}
+
+
