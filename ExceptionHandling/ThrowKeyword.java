f4193752a3871e5352dec660bd1c00e5af35c79d
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-05-19 23:35:51 +0530

    Add files via upload

---
+++ ExceptionHandling/ThrowKeyword.java
@@ -0,0 +1,30 @@
+package ExceptionHandling;
+
+public class ThrowKeyword {
+	
+	public static void main(String[] args)
+	{
+		int num1=6;
+		int num2=-2;
+		
+		try
+		{
+			
+		if(num2<0)
+		{
+			Exception e=new ArithmeticException("Negative Number");
+			throw e;
+		}
+		else
+		{
+		int result= num1/num2;
+		System.out.println(result);
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Enter a valid number" + e);
+		}
+	}
+
+}
