d34aa37a3cbd93ef8d27df5339a879c58a1580bd
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-05-28 22:50:14 +0530

    Add files via upload

---
+++ APIandAnnotations/DateTime1.java
@@ -0,0 +1,29 @@
+package APIandAnnotation;
+
+//import javax.swing.plaf.synth.SynthSplitPaneUI;
+
+//import java.util.Date;
+//import java.util.*;
+public class DateTime1 
+{
+    public static void main(String[] args) 
+    {
+        java.util.Date dt=new java.util.Date();
+        System.out.println(dt);
+        
+
+        long timeInMs=dt.getTime();
+
+
+        java.sql.Date dt1=new java.sql.Date(timeInMs);
+        System.out.println(dt1);
+
+
+        //java.util.ArrayList al=new java.util.ArrayList<>();
+
+
+        
+        
+    }
+    
+}
