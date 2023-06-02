0440b72adb826dd6a6e8cee32bb37a85dacc5755
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-06-02 23:06:38 +0530

    Add files via upload

---
+++ FIleHandling/LaunchTS.java
@@ -0,0 +1,52 @@
+package FileHandling;
+import java.io.*;
+
+
+class Cricketer1 implements Serializable
+{
+     String name;
+     transient int age; //transient variable
+     int runs;
+
+    public Cricketer1(String name, int age, int runs)
+    {
+        this.name=name;
+        this.age=age;
+        this.runs=runs;
+
+    }
+    public void disp()
+    {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(runs);
+    }   
+}
+
+
+public class LaunchTS 
+{
+    public static void main(String[] args) throws Exception
+    {
+       
+    	 Cricketer1 c=new Cricketer1("Sachin", 44, 30000);
+         c.disp();
+
+         FileOutputStream fos=new FileOutputStream("mca.txt");
+         BufferedOutputStream bos=new BufferedOutputStream(fos);
+         ObjectOutputStream oos=new ObjectOutputStream(bos);
+
+         oos.writeObject(c);
+
+         oos.flush();
+         
+         FileInputStream fis=new FileInputStream("pw.txt");
+         BufferedInputStream bis=new BufferedInputStream(fis);
+         ObjectInputStream ois=new ObjectInputStream(bis);
+
+         Cricketer1 cr=(Cricketer1)ois.readObject();
+         cr.disp();        
+          
+    }  
+}
+
