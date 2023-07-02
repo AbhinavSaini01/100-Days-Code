4f0bc158fbd34701490a8a31e08393fcd6b907bf
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-07-02 21:57:48 +0530

    Add files via upload

---
+++ Basic_Java_Programs/Factorial.java
@@ -0,0 +1,15 @@
+package BasicPrograms;
+
+class Factorial
+{  
+	 public static void main(String args[])
+	 {  
+	  int i,fact=1;  
+	  int number=5;//number to calculate factorial    
+	  for(i=1;i<=number;i++){    
+	      fact=fact*i;    
+	  }    
+	  System.out.println("Factorial of "+number+" is: "+fact);    
+	 }  
+	}  
+
