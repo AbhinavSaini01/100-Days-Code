82b3c29c171b8da5981fdc3b9a6097353b009c0a
Abhinav Saini <120383320+AbhinavSaini01@users.noreply.github.com>
2023-07-14 05:50:38 +0530

    Add files via upload

---
+++ Basic_Java_Programs/Multiplication_Table.java
@@ -0,0 +1,16 @@
+package BasicPrograms;
+
+import java.util.Scanner;
+public class Multiplication_Table 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+	System.out.print("Enter number:");        
+	int n=s.nextInt();
+        for(int i=1; i <= 10; i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
